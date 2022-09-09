1aa7a7ba7a951b3e478c34b00d3343185c9f2836
Rishabh Pal <68963452+Rishabh-Pal@users.noreply.github.com>
2022-09-10 02:41:24 +0530

    Add files via upload

---
+++ Counter/index.js
@@ -0,0 +1,11 @@
+function increament() {
+    var number = Number(document.getElementById('number').innerHTML)
+    document.getElementById('number').innerHTML = number + 1
+}
+function decreament() {
+    var number = Number(document.getElementById('number').innerHTML)
+    document.getElementById('number').innerHTML = number - 1
+}
+function neutral() {
+    document.getElementById('number').innerHTML = 0
+}
